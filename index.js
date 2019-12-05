9e3f47ab25b674173fed00261b438f23b4c30b5f
vibraniumforge <gregm9999@gmail.com>
2019-12-05 09:40:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+document.addEventListener("DOMContentLoaded", () => {
+  // const username = document.getElementById("name-input").value;
   let joke;
 
-  function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
-      headers: {
-        "Accept": "application/json"
-      }
-    })
-    .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
-  }
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    fetchJoke();
+  });
+});
 
-  form.addEventListener('submit', (event) => {
+const form = document.getElementById("joke-form");
+const jokeList = document.getElementById("joke-list");
+const username = document.getElementById("name-input").value;
 
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+function fetchJoke() {
+  const username = document.getElementById("name-input").value;
+  if (username === "") return;
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json"
+    }
   })
-})
+    .then(res => res.json())
+    .then(joke => appendJoke(joke))
+    // .then(jokeData => (joke = jokeData.joke))
+    .catch(err => console.log(err));
+}
+
+function appendJoke(joke) {
+  const username = document.getElementById("name-input").value;
+  const newJokeLi = document.createElement("li");
+  newJokeLi.innerHTML = `
+    <span class="username">${username} says:</span> ${joke.joke}
+    `;
+  jokeList.appendChild(newJokeLi);
+}
+
+// form.addEventListener("submit", event => {
+//   event.preventDefault();
+//   if (username === "") return;
+//   fetchJoke();
+// newJokeLi.innerHTML = `
+//   <span class="username">${username} says:</span> ${joke}
+//   `;
+// jokeList.appendChild(newJokeLi);
+// });
